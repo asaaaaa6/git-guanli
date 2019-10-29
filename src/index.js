b450082c616c97510114f060dd337f5d991e41a5
asaaaaa6 <2082261466@qq.com>
2019-10-29 20:43:30 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from 'react-router-dom';
+import Main from './components/Main';
+import Login from './components/Login';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 导入antd全局样式。
+import 'antd/dist/antd.css';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文，导入语言包，在<ConfigProvider>组件中使用。
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 导入日期格式化模块moment。
+import moment from 'moment';
+// 导入日期格式化模块的语言包
+import 'moment/locale/zh-cn';
+// 使用语言包
+moment.locale('zh-cn');
+
+function Auth({ location }) {
+    var isLogin = store.getState();
+    return isLogin ? <Main /> : <Redirect to={{ pathname: "/login", state: { from: location } }} />
+}
+
+ReactDOM.render(
+    <ConfigProvider locale={zhCN}>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" render={Auth} />
+                    <Route path="/login"><Login /></Route>
+                </Switch>
+            </Router>
+        </Provider>
+    </ConfigProvider>,
+    document.getElementById('root'));
